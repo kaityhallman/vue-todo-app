bb33e84c1a855eae2617dceafa05495b3d71a4a9
kaityhallman <kaityhallman@gmail.com>
2018-03-20 20:09:19 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="app">
+    <TodoList v-bind:todos="todos" />
+    <CreateTodo v-on:add-todo="addTodo" />
+  </div>
+</template>
+
+<script>
+
+import CreateTodo from './components/CreateTodo';
+import TodoList from './components/TodoList';
+
+export default {
+  name: 'App',
+  components: {
+    TodoList,
+    CreateTodo,
+  },
+  data() {
+    return {
+      todos: [
+        {
+          title: 'Todo A',
+          project: 'Project A',
+          done: false,
+          id: 1,
+        },
+        {
+          title: 'Todo B',
+          project: 'Project B',
+          done: true,
+          id: 2,
+        },
+        {
+          title: 'Todo C',
+          project: 'Project C',
+          done: false,
+          id: 3,
+        },
+        {
+          title: 'Todo D',
+          project: 'Project D',
+          done: false,
+          id: 4,
+        },
+      ],
+    };
+  },
+  methods: {
+    addTodo(title) {
+      this.todos.push({
+        title,
+        done: false,
+      });
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+  <div>
+    <p>Completed tasks: {{todos.filter(todo => todo.done === true).length}}</p>
+    <p>Pending tasks: {{todos.filter(todo => todo.done === false).length}}</p>
+    <Todo
+      v-on:delete-todo="deleteTodo"
+      v-for="todo in todos"
+      v-bind:todo="todo"
+      :key="todo.id"
+    />
+  </div>
+</template>
+
+<script type="text/javascript">
+import Todo from './Todo';
+
+export default {
+  props: ['todos'],
+  components: {
+    Todo,
+  },
+  methods: {
+    deleteTodo(todo) {
+      const todoIndex = this.todos.indexOf(todo);
+      this.todos.splice(todoIndex, 1);
+    },
+  },
+};
+</script>
+
+<style lang="css">
+</style>
